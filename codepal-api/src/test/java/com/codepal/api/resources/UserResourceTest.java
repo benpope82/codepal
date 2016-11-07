f348170ad142b3fbdb751dd0f156697deb4a9ef4
stephenhu3 <stephenhu3@gmail.com>
2016-11-06 16:49:32 -0800

    added additional tests

--- codepal-api/src/test/java/com/codepal/api/resources/UserResourceTest.java
+++ codepal-api/src/test/java/com/codepal/api/resources/UserResourceTest.java
@@ -1,6 +1,9 @@
 package com.codepal.api.resources;
 
+import com.codepal.api.core.AccessToken;
 import com.codepal.api.core.UserSearch;
+import com.datastax.driver.core.BoundStatement;
+import com.datastax.driver.core.PreparedStatement;
 import com.datastax.driver.core.ResultSet;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.jackson.Jackson;
@@ -30,7 +33,7 @@ public class UserResourceTest {
 
     @ClassRule
     public static CassandraCQLUnit cassandraCQLUnit = new CassandraCQLUnit(
-            new ClassPathCQLDataSet("fixtures/setup-test-tables.cql","codepal_test"),
+            new ClassPathCQLDataSet("fixtures/setup-test-tables.cql", "codepal_test"),
             EmbeddedCassandraServerHelper.DEFAULT_CASSANDRA_YML_FILE, 100000L, 10000);
 
     @BeforeClass
@@ -86,4 +89,48 @@ public class UserResourceTest {
         thrown.expect(NullPointerException.class);
         UserSearch actualResult = userResource.searchUser(searchByUserId);
     }
+
+    @Test
+    public void updateAccessTokenSuccess() throws IOException {
+        AccessToken updateAccessToken = MAPPER.readValue(
+                fixture("fixtures/update-access-token-success-request-response.json"), AccessToken.class);
+
+        // check precondition
+        PreparedStatement statement = cassandraCQLUnit.session.prepare(
+                "SELECT accessToken FROM users WHERE userId = :userId;"
+        );
+        BoundStatement precondition = new BoundStatement(statement);
+        ResultSet preresult = cassandraCQLUnit.session.execute(precondition.bind()
+                .setString("userId", updateAccessToken.getUserId()));
+        assertThat(preresult.iterator().next().getString("accessToken"),
+                is("2pH1jZNZZQyIlavi1dkg0pZj0WDdQlAXNZwkfNFmuADbyXmoCbMCXcrOADJCK1ew2GvAjBvBj88KjMAjwOZAX5YiEbNZHj7iK" +
+                        "miFy8JsiCDwdyvM8bq9ZCPXZyfZs4hBI8HdMaBYAz7lJEQkHgrcVogpMKyI3KNA2p5kZWFDVxLaTPsggU0R"));
+
+        // check postcondition
+        AccessToken actualResult = userResource.updateAccessToken(updateAccessToken);
+
+        BoundStatement postcondition = new BoundStatement(statement);
+        ResultSet postResult = cassandraCQLUnit.session.execute(postcondition.bind()
+                .setString("userId", updateAccessToken.getUserId()));
+        assertThat(postResult.iterator().next().getString("accessToken"),
+                is("FmvWKsQZAvX4CgJIZQbA7mlegyZKDKj1HJM8gZijDIkZjMihxCa0liHblaM8yVEXrFDDZpg2pzXdkNMCHZBByuXABT1HddUp0" +
+                        "2jg39wbX8AFk2yabwc1kWAoBNfZfNLG5sOjAqdPQ05vrYCOpKyyi8KM7vRNcjEDomZZjiVsJIANCww8PAYZ"));
+
+        AccessToken expectedResult = MAPPER.readValue(
+                fixture("fixtures/update-access-token-success-request-response.json"), AccessToken.class);
+
+        assertTrue(actualResult.equals(expectedResult));
+    }
+
+    // @Test
+    // public void updateAccessTokenFail() throws IOException {
+    //     UserSearch searchByAccessToken = MAPPER.readValue(
+    //             fixture("fixtures/search-by-access-token-success-request.json"), UserSearch.class);
+    //
+    //     UserSearch actualResult = userResource.searchUser(searchByAccessToken);
+    //     UserSearch expectedResult = MAPPER.readValue(
+    //             fixture("fixtures/search-user-success-response.json"), UserSearch.class);
+    //
+    //     assertTrue(actualResult.equals(expectedResult));
+    // }
 }
